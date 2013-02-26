23d24ccfa25ed62e1ce1d98b0fd71aa3f9810ea1
Eduardo Avila <edu.avila2@gmail.com>
2013-02-26 10:41:56 -0400

    Implemetraçao do cadastro de produtos.

---
+++ sistemaCA/sistemaCA/views/produtos/FormProdutosCadastro.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace sistemaCA.views.produtos
+{
+    partial class FormProdutosAtualizar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_descricao = new System.Windows.Forms.TextBox();
+            this.cb_unidademedida = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_tipoproduto = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.Location = new System.Drawing.Point(307, 305);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(77, 30);
+            this.btn_cadastrar.TabIndex = 0;
+            this.btn_cadastrar.Text = "Cadastrar";
+            this.btn_cadastrar.UseVisualStyleBackColor = true;
+            this.btn_cadastrar.Click += new System.EventHandler(this.btn_cadastrar_Click);
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Location = new System.Drawing.Point(419, 305);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(80, 30);
+            this.btn_cancelar.TabIndex = 1;
+            this.btn_cancelar.Text = "Cadastrar";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // tb_nome
+            // 
+            this.tb_nome.Location = new System.Drawing.Point(28, 57);
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.Size = new System.Drawing.Size(205, 20);
+            this.tb_nome.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Descrição";
+            // 
+            // tb_descricao
+            // 
+            this.tb_descricao.Location = new System.Drawing.Point(255, 57);
+            this.tb_descricao.Name = "tb_descricao";
+            this.tb_descricao.Size = new System.Drawing.Size(271, 20);
+            this.tb_descricao.TabIndex = 4;
+            // 
+            // cb_unidademedida
+            // 
+            this.cb_unidademedida.FormattingEnabled = true;
+            this.cb_unidademedida.Location = new System.Drawing.Point(29, 114);
+            this.cb_unidademedida.Name = "cb_unidademedida";
+            this.cb_unidademedida.Size = new System.Drawing.Size(82, 21);
+            this.cb_unidademedida.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Unidade Medida";
+            // 
+            // cb_tipoproduto
+            // 
+            this.cb_tipoproduto.FormattingEnabled = true;
+            this.cb_tipoproduto.Location = new System.Drawing.Point(255, 114);
+            this.cb_tipoproduto.Name = "cb_tipoproduto";
+            this.cb_tipoproduto.Size = new System.Drawing.Size(146, 21);
+            this.cb_tipoproduto.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(252, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tipo Produto";
+            // 
+            // FormProdutosCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 359);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cb_tipoproduto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cb_unidademedida);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_descricao);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Name = "FormProdutosCadastro";
+            this.Text = "Cadastro Produto";
+            this.Load += new System.EventHandler(this.FormProdutosCadastro_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_descricao;
+        private System.Windows.Forms.ComboBox cb_unidademedida;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cb_tipoproduto;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ sistemaCA/sistemaCA/views/produtos/FormProdutosCadastro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using sistemaCA;
+namespace sistemaCA.views.produtos
+{
+    public partial class FormProdutosAtualizar : Form
+    {
+       
+
+        public FormProdutosAtualizar()
+        {
+            InitializeComponent();
+
+            
+        }
+
+      
+
+        private void FormProdutosCadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cadastrar_Click(object sender, EventArgs e)
+        {
+
+
+            Produto produto = new Produto();
+            produto.Nome = tb_nome.Text;
+            produto.Descricao = tb_descricao.Text;
+            produto.UnidadeMedida = cb_unidademedida.Text;
+            produto.Id_tipoproduto = int.Parse(cb_tipoproduto.Text);
+
+            produto.CadastarProduto();
+
+
+            //produto.descricao tb_descricao.Text = produto.descricao;
+            //tb_nome.Text = produto.nome;
+            //cb_unidademedida. = produto.unidade_medida;
+            
+
+        }
+    }
+}

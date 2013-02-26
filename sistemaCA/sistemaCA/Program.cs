450f095423a81d0695f2000e75b1f792a505c1fc
Eduardo Avila <edu.avila2@gmail.com>
2013-02-25 20:34:26 -0400

    auteração automatica do visual studio  do projeto

--- sistemaCA/sistemaCA/Program.cs
+++ sistemaCA/sistemaCA/Program.cs
@@ -16,7 +16,7 @@ namespace sistemaCA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new form1());
+            Application.Run(new telaspash());
         }
     }
 }

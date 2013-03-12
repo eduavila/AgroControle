68709221c1faf971066d591fc17cb90414fd72b9
Eduardo Avila <edu.avila2@gmail.com>
2013-03-12 10:45:40 -0400

    implementado  deletaa produtos da aplicação.

--- sistemaCA/sistemaCA/Modulos/aplicacao/ProdutoAplicado.cs
+++ sistemaCA/sistemaCA/Modulos/aplicacao/ProdutoAplicado.cs
@@ -26,6 +26,54 @@ namespace sistemaCA.Modulos.aplicacao
             Produto = new tblproduto();
         }
 
+        // listando produto dentro do datagrid.
+        public  void ListandoTodoProduto(int idaplicacao,DataGridView dgw)
+        {
+            var result = from produtoaplica in Banco.tblprodutosaplicados
+                         join produto in Banco.tblprodutos on produtoaplica.id_produto equals produto.id_produto
+                         where produtoaplica.id_aplicacao == idaplicacao
+                         select new
+                         {
+                             idproduto = produto.id_produto,
+                             produto = produto.descricao,
+                             quantidade = produtoaplica.quantidade,
+                             valor = produtoaplica.preco
+                             
+                         };
+
+
+
+            var pro = from produ in Banco.tblprodutos select produ.descricao;
+
+            dgw.AutoGenerateColumns = false;
+            
+
+            DataGridViewComboBoxColumn cb_produto = new DataGridViewComboBoxColumn();
+            cb_produto.DataPropertyName = "produto";
+            cb_produto.DisplayMember = "produto";
+            cb_produto.ValueMember = "idproduto";
+            cb_produto.DataSource = result.ToList();
+            cb_produto.HeaderText = "Produto";
+
+            DataGridViewTextBoxColumn tb_valor = new DataGridViewTextBoxColumn();
+            tb_valor.DataPropertyName = "valor";
+            tb_valor.HeaderText = "Valor";
+
+            DataGridViewTextBoxColumn tb_quantidade = new DataGridViewTextBoxColumn();
+            tb_quantidade.DataPropertyName = "quantidade";
+            tb_quantidade.HeaderText = "Quantidade";
+
+
+            dgw.Columns.Add(cb_produto);
+            dgw.Columns.Add(tb_quantidade);
+            dgw.Columns.Add(tb_valor);
+
+            dgw.DataSource = result;
+
+            
+            
+        
+        }
 
 
         public void ListandoProduto(DataGridView dgw)
@@ -104,5 +152,28 @@ namespace sistemaCA.Modulos.aplicacao
 
         
         }
+
+
+        public void DeletandoTodoProduto(int idaplicacao)
+        {
+            try
+            {
+                var result = from produtos in Banco.tblprodutosaplicados where produtos.id_aplicacao == idaplicacao select produtos;
+
+
+ 
+                Banco.tblprodutosaplicados.DeleteAllOnSubmit(result);
+                Banco.SubmitChanges();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        
+        }
+
+
+
+
     }
 }

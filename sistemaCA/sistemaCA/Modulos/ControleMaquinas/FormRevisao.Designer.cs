44a985e0f0380358a72d43f8eeb3f5dabd527798
Eduardo Avila <edu.avila2@gmail.com>
2013-03-15 10:22:57 -0400

    add formulario e deletado  previsao de revisao.

--- sistemaCA/sistemaCA/Modulos/ControleMaquinas/FormRevisao.Designer.cs
+++ sistemaCA/sistemaCA/Modulos/ControleMaquinas/FormRevisao.Designer.cs
@@ -28,11 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tb_pesquisar = new System.Windows.Forms.MaskedTextBox();
+            this.cb_seleciona = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.btn_visualizar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgw_revisao = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_revisao)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.tb_pesquisar);
+            this.groupBox3.Controls.Add(this.cb_seleciona);
+            this.groupBox3.Location = new System.Drawing.Point(574, 18);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(273, 64);
+            this.groupBox3.TabIndex = 50;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Pesquisar";
+            // 
+            // tb_pesquisar
+            // 
+            this.tb_pesquisar.Location = new System.Drawing.Point(89, 24);
+            this.tb_pesquisar.Name = "tb_pesquisar";
+            this.tb_pesquisar.Size = new System.Drawing.Size(178, 20);
+            this.tb_pesquisar.TabIndex = 43;
+            // 
+            // cb_seleciona
+            // 
+            this.cb_seleciona.FormattingEnabled = true;
+            this.cb_seleciona.Items.AddRange(new object[] {
+            "ID",
+            "Nome",
+            "CPF"});
+            this.cb_seleciona.Location = new System.Drawing.Point(12, 24);
+            this.cb_seleciona.Name = "cb_seleciona";
+            this.cb_seleciona.Size = new System.Drawing.Size(70, 21);
+            this.cb_seleciona.TabIndex = 42;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.btn_cadastrar);
+            this.groupBox2.Controls.Add(this.btn_visualizar);
+            this.groupBox2.Location = new System.Drawing.Point(7, 17);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(341, 64);
+            this.groupBox2.TabIndex = 49;
+            this.groupBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 40;
+            this.button1.Text = "Excluir";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.Location = new System.Drawing.Point(13, 16);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(87, 33);
+            this.btn_cadastrar.TabIndex = 34;
+            this.btn_cadastrar.Text = "Cadastrar";
+            this.btn_cadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btn_visualizar
+            // 
+            this.btn_visualizar.Location = new System.Drawing.Point(114, 16);
+            this.btn_visualizar.Name = "btn_visualizar";
+            this.btn_visualizar.Size = new System.Drawing.Size(87, 33);
+            this.btn_visualizar.TabIndex = 39;
+            this.btn_visualizar.Text = "Visualizar";
+            this.btn_visualizar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.dgw_revisao);
+            this.groupBox1.Location = new System.Drawing.Point(8, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(846, 406);
+            this.groupBox1.TabIndex = 48;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Usuário";
+            // 
+            // dgw_revisao
+            // 
+            this.dgw_revisao.AllowUserToAddRows = false;
+            this.dgw_revisao.AllowUserToDeleteRows = false;
+            this.dgw_revisao.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dgw_revisao.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.dgw_revisao.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_revisao.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgw_revisao.Location = new System.Drawing.Point(3, 16);
+            this.dgw_revisao.Name = "dgw_revisao";
+            this.dgw_revisao.ReadOnly = true;
+            this.dgw_revisao.Size = new System.Drawing.Size(840, 387);
+            this.dgw_revisao.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 491);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(861, 22);
+            this.statusStrip1.TabIndex = 51;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // FormRevisao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormRevisao";
+            this.ClientSize = new System.Drawing.Size(861, 513);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormRevisao";
+            this.Text = "Previsão de Revisão";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_revisao)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.MaskedTextBox tb_pesquisar;
+        private System.Windows.Forms.ComboBox cb_seleciona;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.Button btn_visualizar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgw_revisao;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }

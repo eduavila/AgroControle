3eecdf18b91af940d79c3a824340ef849af35531
Eduardo Avila <edu.avila2@gmail.com>
2013-03-15 10:15:36 -0400

    add data set e report do relatorio de apicacao.

---
+++ sistemaCA/sistemaCA/Modulos/ControleMaquinas/FormRevisao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistemaCA.Modulos.ControleMaquinas
+{
+    public partial class FormRevisao : Form
+    {
+        public FormRevisao()
+        {
+            InitializeComponent();
+        }
+    }
+}

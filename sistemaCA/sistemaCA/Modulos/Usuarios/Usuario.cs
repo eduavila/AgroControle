567005550fb123145ded8183186cd41fdcdc19dd
Eduardo Avila <edu.avila2@gmail.com>
2013-03-21 21:24:47 -0400

    add formulario cadastro usuario.

---
+++ sistemaCA/sistemaCA/Modulos/Usuarios/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaCA.Modulos.Usuarios
+{
+    class Usuario
+    {
+    }
+}
